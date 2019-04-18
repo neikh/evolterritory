0ac92806e65e732d3cb96d8a499dfc589be2b8c6
Benoit <benoit.ha@gmail.com>
2019-04-18 16:05:57 +0200

    cool calendar

--- resources/js/pages/Post.vue
+++ resources/js/pages/Post.vue
@@ -20,9 +20,6 @@
                             <input class="form-control" id="inputCodePostal" placeholder="Code postal">
                         </div>
                     </div>
-
-
-
                     <div class="form-group col-md-6">
                         <label for="inputlatitude">Latitude</label>
                         <input type="latitude" class="form-control" id="inputLatitude" value="25">
@@ -31,18 +28,24 @@
                         <label for="inputLongitude">Longitude</label>
                         <input type="longitude" class="form-control" id="inputLongitude" value="40">
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="inputDate1">Date 1</label>
-                        <input type="date" class="form-control" id="inputDate1" value="2016-02-01">
+
+                    <div class="row justify-content-center">
+                        <div class="form-group col-md-6">
+                            <label for="inputDate1">Date 1</label>
+                            <datetime v-model="date1" input-id="inputDate1"></datetime>
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="inputDate2">Date 2</label>
+                            <datetime v-model="date2" input-id="inputDate2"></datetime>
+                        </div>
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="inputDate2">Date 2</label>
-                        <input type="date" class="form-control" id="inputDate2" value="2015-02-01">
-                    </div>
+
                     <div class="form-group col-md-12">
                         <label for="formControlRange">Example Range input</label>
                         <input type="range" class="form-control-range" id="formControlRange">
                     </div>
+
                     <div class="row justify-content-center">
                         <button v-on:click="sub()" type="button" name="button" class="btn btn-dark">Chercher</button>
                     </div>
@@ -54,6 +57,8 @@
                 </div>
             </nav>
             <div class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <div id="date1" class="d-inline font-weight-bold"></div>
+                <div id="date2" class="d-inline font-weight-bold float-right"></div>
                 <image-compare class="img-fluid" :before="before" :after="after" :padding="{ left: 50, right: 50 }">
                     <i class="fa fa-angle-left" aria-hidden="true" slot="icon-left"></i>
                     <i class="fa fa-angle-right" aria-hidden="true" slot="icon-right"></i>
@@ -64,19 +69,19 @@
 </template>
 
 <script>
-    import vueLoader from '@nulldreams/vue-loading/src/vue-loading'
+    import vueLoader from '@nulldreams/vue-loading/src/vue-loading';
     export default {
         components: {
             vueLoader
         },
         data () {
             return {
-                before: './img/before.jpg',
-                after: './img/after.jpg'
+                before: '',
+                after: ''
             }
         },
         mounted() {
-
+            console.log("1");
         },
 
         methods:{
@@ -100,10 +105,16 @@
                 }
 
                 let date1 = document.getElementById('inputDate1').value;
-                let img1 = this.getImage(latitude, longitude, date1);
-
                 let date2 = document.getElementById('inputDate2').value;
+
+                date1 = await this.dateRefactor(date1);
+                date2 = await this.dateRefactor(date2);
+
+                let img1 = this.getImage(latitude, longitude, date1);
+                document.getElementById('date1').textContent = date1;
+
                 let img2 = this.getImage(latitude, longitude, date2);
+                document.getElementById('date2').textContent = date2;
 
                 this.before = await img1;
                 this.after = await img2;
@@ -160,6 +171,26 @@
                 let coordinates = [response.data[0].lat, response.data[0].lon];
 
                 return coordinates;
+            },
+
+            dateRefactor(date){
+                let months = ['janv.','févr.','mar.','avr.','mai','juin','juil.','août','sep.','oct.','nov.','dec.'];
+
+                let organize = date.split(' ');
+                let day = organize[0];
+                let month = organize[1];
+                let year = organize[2];
+
+                day = (day < 10 ? '0'+day : day);
+
+                for (let i = 0; i < months.length; i++){
+                    if (month === months[i]){
+                        month = i + 1;
+                        month = (month < 10 ? '0'+month : month);
+                    }
+                }
+
+                return year+"-"+month+"-"+day;
             }
         }
      }
