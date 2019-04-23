0b38867370cc822779d3274d80bfd0be8165f385
irealg <c.vincent.faller@gmail.com>
2019-04-23 15:25:32 +0200

    marge

--- resources/js/pages/Flow.vue
+++ resources/js/pages/Flow.vue
@@ -1,101 +1,35 @@
 <template>
-    <!-- <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 row">
-                <nav class="navbar navbar-light bg-light">
-                    <form class="form-inline">
-                        <button class="btn btn-outline-success" type="button">Hot</button>
-                        <button class="btn btn-outline-success" type="button">New</button>
-                    </form>
-                </nav>
-            </div>
-        </div>
-        <hr>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <img class="img-fluid" src="https://apod.nasa.gov/apod/image/1904/JupiterDolphin_JunoEichstadt_1080.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
     <div>
-        <b-tabs content-class="mt-3" justified>
-            <b-tab title="Hot" active>
-                <div class="container d-flex">
-
-
-                <div class="card-deck mb-3 text-center">
-                    <div class="card mb-4 shadow-sm">
-                        <div class="card-header">
-aaaaaa
-                        </div>
-                        <div class="card-body">
-aaaa
-                        </div>
-                        <div class="card-footer">
-aaaaaaa
-                        </div>
-                    </div>
-                    <div class="card mb-4 shadow-sm">
-                        <div class="card-header">
-aaaaaa
-                        </div>
-                        <div class="card-body">
-aaaa
-                        </div>
-                        <div class="card-footer">
-aaaaaaa
-                        </div>
-                    </div>
-                    <div class="card mb-4 shadow-sm">
-                        <div class="card-header">
-aaaaaa
-                        </div>
-                        <div class="card-body">
-aaaa
-                        </div>
-                        <div class="card-footer">
-aaaaaaa
-                        </div>
-                    </div>
-                    <div class="card mb-4 shadow-sm">
-                        <div class="card-header">
-aaaaaa
-                        </div>
-                        <div class="card-body">
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-                        </div>
-                        <div class="card-footer">
-aaaaaaa
-                        </div>
-                    </div>
-
-                </div>
-                </div>
-            </b-tab>
-            <b-tab title="New">
-
-            </b-tab>
-        </b-tabs>
+        <div v-for="item in response.data">
+            <flowItem :titre="titre" :description="description" ></flowItem>
+        </div>
     </div>
 </template>
 
 <script>
-
+    import flowItem from '../components/flowElements';
     export default {
-        mounted() {
-            console.log('Component mounted.')
+       item: null,
+        components: {
+           flowItem
+        },
+
+        data () {
+            return {
+               response: '',
+               titre: '',
+               description: ''
+            }
+        },
+
+        async mounted() {
+             this.response = await axios.get('flow/new')
+             this.item = response.data;
         }
     }
 </script>
 <style>
-.card{
-    /* min-height: 200px; */
+/* .card{
     min-width: 300px;
-}
+} */
 </style>
