56ec846229cb1ca427cf9703a0f559f1a770b323
Benoit <benoit.ha@gmail.com>
2019-04-23 13:52:21 +0200

    snake konami

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,36 +5,34 @@ import VueSweetalert2 from 'vue-sweetalert2';
 // You need a specific loader for CSS files
 import 'vue-datetime/dist/vue-datetime.css';
 import VueImageCompare from 'vue-image-compare';
- 
+import KonamiCode from 'vue-konami-code'
+
 const options = {
   confirmButtonColor: '#343a40',
   cancelButtonColor: '#ff7674'
 }
- 
+
 Vue.use(VueSweetalert2, options)
 
 Vue.use(Datetime);
 Vue.use(VueImageCompare);
 
+Vue.use(KonamiCode, {callback: function () {
+    Vue.swal.fire({
+        html:
+        '<iframe id="inlineFrameExample"' +
+        'title="Inline Frame Example"' +
+        'width="1600"' +
+        'height="750"' +
+        'src="snake/index.html">' +
+        '</iframe>',
+        showConfirmButton: false,
+        width: 1920,
+        height: 1080
+    })
+}})
+
 require('./bootstrap');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const index = new Vue({
     el: '#index',
